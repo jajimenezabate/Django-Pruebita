b7a047ff018e5a6dbd17cf6d249c28de5d34a9d4
jajimenezabate <101368145+jajimenezabate@users.noreply.github.com>
2022-06-15 01:37:09 -0400

    Add files via upload

---
+++ pruebita/pruebita/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+
+def inicio(request):
+    doc_externo = open("static/index.html")
+    plt = Template(doc_externo.read())
+    doc_externo.close()
+    ctx = Context()
+    documento = plt.render(ctx)
+    return HttpResponse(documento)
